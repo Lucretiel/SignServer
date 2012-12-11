e3453dc44020361fc21927a47a6b769f8a3be433
Nathan West <Lucretiel@gmail.com>
2012-12-11 13:38:00 -0500

    Added wrapper code to main()

--- src/main.py
+++ src/main.py
@@ -4,13 +4,19 @@ Created on Dec 11, 2012
 @author: nathan
 '''
 
+from time import sleep
+
 from handler import Handler
-from BaseHTTPServer import HTTPServer
+from threaded_server import ThreadedServer
 
 def main():
     
-    server = HTTPServer(('localhost', 8888), Handler)
-    server.serve_forever()
+    try:
+        with ThreadedServer(('localhost', 8888), Handler):
+            while True:
+                sleep(10)
+    except KeyboardInterrupt as e:
+        print "Server stopped due to keyboard interrupt:\n%s" % repr(e)
 
 if __name__ == '__main__':
     main()
