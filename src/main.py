16cbc9181949362689b5d21c4033751b5732ce8e
Nathan West <Lucretiel@gmail.com>
2012-12-13 12:27:14 -0500

    Added --debug

--- src/main.py
+++ src/main.py
@@ -6,18 +6,20 @@ Created on Dec 12, 2012
 
 import argparse
 
-from bottle import run
-import app
+import bottle
 
+import app
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--host', default='0.0.0.0')
     parser.add_argument('--port', default='39999', type=int)
     parser.add_argument('--live', action='store_true', help='Autoupdate the server with code changes')
+    parser.add_argument('--debug', action='store_true', help='Print tracebacks on errors')
     
     options = parser.parse_args()
-    run(app.app, host=options.host, port=options.port, reloader=options.live)
+    bottle.debug(options.debug)
+    bottle.run(app.app, host=options.host, port=options.port, reloader=options.live, )
 
 if __name__ == '__main__':
     main()
