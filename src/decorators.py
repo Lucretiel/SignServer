d20951fc2f2132d639d7a8fa2320e61ec8f1623b
Nathan West <Lucretiel@gmail.com>
2012-12-13 14:29:22 -0500

    Made the refactoring behavior into a decorator renamed caching decorators

---
+++ src/decorators.py
@@ -0,0 +1,55 @@
+'''
+Created on Dec 12, 2012
+
+@author: nathan
+'''
+
+from time import time
+
+class Cached(object):
+    def __init__(self, func, timeout):
+        self.func = func
+        self.value = None
+        self.value_time = 0
+        self.timeout = timeout
+        
+    def __call__(self, *args, **kwargs):
+        if time() - self.value_time > self.timeout or self.value == None:
+            self.value = self.func(*args)
+        self.value_time = time()
+        return self.value
+    def clear_cache(self):
+        self.value = None
+            
+def cached(func):
+    '''Decorator to make functions cache their value. Converts function into
+    an object of type Cached, above. Set the timeout with timout and reset
+    the cache with clear_cache(). None is not cached.
+    '''
+    return Cached(func, 0)
+
+def retrying(default):
+    '''Decorator to make functions retry a certain number of times before
+    returning a value. returning None is considered a failure. The decorated
+    function will use the retries kwarg to know how many times to retry, and
+    default to default
+    '''
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            if 'retries' in kwargs:
+                retries = kwargs['retries']
+                del kwargs['retries']
+            else:
+                retries = default
+            result = None
+            if retries == 0:
+                while result is None:
+                    result = func(*args, **kwargs)
+            else:
+                for _ in xrange(retries):
+                    result = func(*args, **kwargs)
+                    if result is not None:
+                        break
+            return result
+        return wrapper
+    return decorator
--- src/sign.py
+++ src/sign.py
@@ -6,22 +6,22 @@ Created on Dec 13, 2012
 
 import alphasign
 
-from caching import cached
+from decorators import cached, retrying
 
 sign = alphasign.Serial()
 sign.connect()
 sign_memory_timeout = 60 * 10 #How long until the memory cache dies
-sign_retries = 3 #Number of times 
+sign_retries = 3 #Default number of times to retry getting memory. 0 for forever.
 
 @cached
+@retrying(sign_retries)
 def read_raw_memory_table(retries=None):
-    if retries is None:
-        retries = sign_retries
-        
-    for _ in xrange(retries):
-        table = sign.read_raw_memory_table()
-        if table is not False:
-            return table
-    return None
+    '''Caching, retrying version of sign.read_raw_memory_table. If the result is
+    not None, preserves the result for `timeout` seconds. `timeout` is an
+    instance variable of this function.
+    '''
+    
+    table = sign.read_raw_memory_table()
+    return table if table is not False else None
 
 read_raw_memory_table.timeout = sign_memory_timeout
