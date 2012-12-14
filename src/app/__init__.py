de0ae8b54ecf1ffa9d3b4b808bc1b0f6267d995d
Nathan West <Lucretiel@gmail.com>
2012-12-14 14:52:54 -0500

    Added copyright.

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -3,7 +3,24 @@ Created on Dec 12, 2012
 
 @author: nathan
 
-This file manages miscellaneous stuff for the Bottle object 
+This file manages miscellaneous stuff for the Bottle object
+
+Copyright 2012, 2013 Nathan G. West
+
+This file is part of SignServer.
+
+SignServer is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+SignServer is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with SignServer.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
 import bottle
--- src/constants.py
+++ src/constants.py
@@ -3,7 +3,22 @@ Created on Dec 6, 2012
 
 @author: nathan
 
-Fixed functionality related to sign file labels. Validation, etc
+Copyright 2012, 2013 Nathan G. West
+
+This file is part of SignServer.
+
+SignServer is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+SignServer is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with SignServer.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
 from alphasign import colors, modes
